8c0d74ef258b6ca99ea34432e2ddde866940e5ea
sathya <sathya@Ashwanths-MacBook-Pro.local>
2015-06-29 11:52:38 -0700

    Added

---
+++ MaxNum.py
@@ -0,0 +1,11 @@
+a =9
+b = 10
+def get_max(a, b):
+    # Write your code here
+    # To print results to the standard output you can use print
+    # Example: print "Hello world!"
+    if a > b:
+        print a
+    else:
+        print b
+
