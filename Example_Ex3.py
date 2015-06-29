1e1e0e0abaa96bcb3e4bff9824b43e0eb258ea76
sathya <sathya@Ashwanths-MacBook-Pro.local>
2015-06-29 11:25:36 -0700

    Added

---
+++ Example_Ex3.py
@@ -0,0 +1,13 @@
+'''
+Functional programming foundations in PythonTry Me!
+Start with an array of integers, 1 through 5. First,
+square all the elements in the array. Then, sum all
+the elements of the squared array.
+'''
+
+def square_all(l1):
+    for i in l1:
+        print i * i
+
+square_all(range(1,5))
+
